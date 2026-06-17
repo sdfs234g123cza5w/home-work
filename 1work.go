acc28555e4edc11b766df72a9c6d9b895543767f
Yermekkaliyev Zhannur <goharer1@gmail.com>
2026-06-17 13:02:27 +0500

    Add files via upload

---
+++ 1work.go
@@ -0,0 +1,26 @@
+schooling := 11
+
+fmt.Println(schooling)
+
+schooling = 12
+
+fmt.Println(schooling)
+
+name := "Vladislav"
+
+fmt.Println(name)
+
+name = "Zhannur"
+
+steps := 0
+fmt.Println(steps)
+
+steps = 2000
+fmt.Println(steps)
+fmt.Println("Хорошая работа! Вы уже на пути к своей ежедневной цели")
+
+largeNumber := 6000000
+fmt.Println(largeNumber)
+
+const breakTime = 15
+fmt.Println(breakTime)
