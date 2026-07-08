507a36ad1186c0581c2e2fc6b34ceb4e0ed9f6e0
Yermekkaliyev Zhannur <goharer1@gmail.com>
2026-07-08 23:35:52 +0500

    Add files via upload

--- 3work.go
+++ 3work.go
@@ -1,52 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	bannerWidth := 12
-	bannerHeight := 8
-	bannerArea := bannerWidth * bannerHeight
-	fmt.Println(bannerArea)
-	halfBannerArea := bannerArea / 2
-	fmt.Println(halfBannerArea)
-	bannerBorderLength := (bannerWidth + bannerHeight) * 2
-	fmt.Println(bannerBorderLength)
-	boxCount := 29
-	leftoverBoxes := boxCount % 5
-	fmt.Println(leftoverBoxes)
-	tempMorning := 15
-	tempAfternoon := 25
-	tempEvening := 20
-	totalTemp := tempMorning + tempAfternoon + tempEvening
-	averageTemp := totalTemp / 3
-	fmt.Println(averageTemp)
-	knownWords := 47
-	wordsGoal := 120
-	progressPercent := (float64(knownWords) / float64(wordsGoal)) * 100
-	fmt.Println(progressPercent, "%")
-	coins := 0
-	coins += 500
-	fmt.Println(coins)
-	coins += 1200
-	fmt.Println(coins)
-	coins /= 2
-	fmt.Println(coins)
-	coins *= 2
-	fmt.Println(coins)
-	coins -= 300
-	fmt.Println(coins)
-	participants := 42
-	groupCount := 8
-	participantsPerGroup := participants / groupCount
-	fmt.Println(participantsPerGroup)
-	fmt.Println(20 - 4 * 3)
-	fmt.Println((20 - 4) * 3)
-	squareValue := 81.0
-	fmt.Println(math.Sqrt(squareValue))
-	multiplier := 5.0
-	exponent := 2.0
-	fmt.Println(math.Pow(multiplier, exponent))
+  var name string
+  var weight float64
+  var distance float64
+  var things int
+
+
+
+BaseRate := 5.50
+TaxRate := 0.12
+DistanceRate := 2.0
+FragileFee := 0.2
+fmt.Print("Введите имя: ")
+fmt.Scan(&name)
+fmt.Println("Введите вес товара: ")
+fmt.Scan(&weight)
+fmt.Println("Введите дистанцию: ")
+fmt.Scan(&distance)
+fmt.Println("Введите количество хрупких товаров: ")
+fmt.Scan(&things)
+
+baseCost := (weight * BaseRate) * (1 + FragileFee * float64(things)) + (distance * DistanceRate)
+totalCost := baseCost * (1 + TaxRate)
+
+fmt.Println("\n--- Отчет о доставке ---")
+fmt.Println("Отправитель:", name)
+fmt.Printf("Итоговая стоимость: %.2f\n", totalCost)
 }
