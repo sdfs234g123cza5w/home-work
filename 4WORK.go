d8a628279aff605aa6bc1f2e21064da7d0269d52
Yermekkaliyev Zhannur <goharer1@gmail.com>
2026-08-05 13:21:50 +0500

    Update 4WORK.go

--- 4WORK.go
+++ 4WORK.go
@@ -3,48 +3,60 @@ package main
 import "fmt"
 
 func main() {
+	// true
 	fmt.Println(5 == 5)
-
+	
+	// true
 	fmt.Println(10 != 3)
-
+	
+	// false
 	fmt.Println(7 > 12)
-
+	
+	// true
 	fmt.Println(15 < 20)
-
+	
+	// true
 	fmt.Println(8 >= 8)
-
+	
+	// false
 	fmt.Println(6 <= 4)
-
+	
+	// false
 	fmt.Println((10 > 5) && (3 < 1))
-
+	
+	// true
 	fmt.Println((10 > 5) || (3 < 1))
-
+	
+	// false
 	fmt.Println(!(5 == 5))
-
+	
+	// true
 	fmt.Println(!(7 < 3))
-
+	
+	// false
 	fmt.Println(true && false)
-
+	
+	// false
 	fmt.Println(false || false)
-
+	
+	// true
 	fmt.Println(true || false)
-
+	
+	// true
 	fmt.Println((4 + 6 == 10) && (9 > 2))
-
+	
+	// true
 	fmt.Println((12 / 3 == 4) || (8 < 5))
 
 	age := 20
 	hasTicket := true
-
+	// true
 	canEnter := age >= 18 && hasTicket
-
 	fmt.Println("Can enter:", canEnter)
 
 	isLoggedIn := true
-  
 	isAdmin := false
-
+	// true
 	hasAccess := (isLoggedIn && isAdmin) || (isLoggedIn && !isAdmin)
-
 	fmt.Println("Has access:", hasAccess)
 }
